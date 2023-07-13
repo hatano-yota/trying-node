765fee14d715cc702c475016151506b7c51e2208
unreact-hatano <mizutama.hahaha@gmail.com>
2023-07-13 12:49:54 +0900

    otherページ追加

--- app.js
+++ app.js
@@ -4,23 +4,33 @@ const url = require("url");
 const ejs = require("ejs");
 
 const index_page = fs.readFileSync("./index.ejs", "utf-8");
+const other_page = fs.readFileSync("./other.ejs", "utf-8");
 const style_css = fs.readFileSync("./style.css", "utf-8");
 
 const getFromClient = (req, res) => {
   const url_parts = url.parse(req.url);
   switch (url_parts.pathname) {
     case "/":
-      const content = ejs.render(index_page, {
+      const index_content = ejs.render(index_page, {
         title: "Indexページ",
         content: "これはテンプレートを使用したサンプルページです。",
       });
       res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(content);
+      res.write(index_content);
       res.end();
       break;
     case "/style.css":
       res.writeHead(200, { "Content-Type": "text/css" });
       res.write(style_css);
+      res.end();
+      break;
+    case "/other":
+      const other_content = ejs.render(other_page, {
+        title: "Other",
+        content: "これはアザーページです。よろしく。",
+      });
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(other_content);
       res.end();
       break;
     default:
