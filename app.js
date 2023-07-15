a4b7793f48a11e3ecf784e89424fe67e6306c870
unreact-hatano <mizutama.hahaha@gmail.com>
2023-07-16 08:25:04 +0900

    フォームの実装

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const qs = require("querystring");
 const ejs = require("ejs");
 
 const index_page = fs.readFileSync("./index.ejs", "utf-8");
@@ -9,27 +10,67 @@ const style_css = fs.readFileSync("./style.css", "utf-8");
 
 const getFromClient = (req, res) => {
   const url_parts = url.parse(req.url, true);
-  let content;
-  let query;
   switch (url_parts.pathname) {
     case "/":
-      content = "これはテンプレートをindexページです。";
-      query = url_parts.query;
-      if (query.msg !== undefined) {
-        content += "あなたは、「" + query.msg + "」と送りました";
-      }
-      content = ejs.render(index_page, {
-        title: "Index",
-        content: content,
-      });
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(content);
+      response_index(req, res);
+      break;
+    case "/other":
+      response_other(req, res);
+      break;
+    case "/style.css":
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.write(style_css);
       res.end();
       break;
     default:
       res.writeHead(200, { "Content-Type": "text/plain" });
       res.end("no page...");
       break;
+  }
+};
+
+const response_index = (req, res) => {
+  const msg = "これはIndexページです。";
+  const content = ejs.render(index_page, {
+    title: "index_page",
+    content: msg,
+  });
+  res.writeHead(200, { "Content-type": "text/html" });
+  res.write(content);
+  res.end();
+};
+
+const response_other = (req, res) => {
+  let msg = "これはOtherページです。";
+  // POSTアクセス処理
+  if (req.method === "POST") {
+    let body = "";
+    // データ受信のイベント処理
+    req.on("data", (data) => {
+      body += data;
+    });
+    // データ受信終了のイベント処理
+    req.on("end", () => {
+      const post_data = qs.parse(body); // データのパース
+      msg += "あなたは、「" + post_data.msg + "」と書きました。";
+      const content = ejs.render(other_page, {
+        title: "Other",
+        content: msg,
+      });
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(content);
+      res.end();
+    });
+  } else {
+    // GETアクセス処理
+    msg = "ページがありません";
+    const content = ejs.render(other_page, {
+      title: "Other",
+      content: msg,
+    });
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(content);
+    res.end();
   }
 };
 
