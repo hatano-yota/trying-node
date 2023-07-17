fd1ebed48e2f67c5a9de3d06a4f737650a5765de
unreact-hatano <mizutama.hahaha@gmail.com>
2023-07-17 22:48:47 +0900

    express入門　formの作成

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -4,7 +4,16 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   const data = {
     title: "Hello!",
-    content: "これは、サンプルのコンテンツです。<br>this is sample content.",
+    content: "※何か書いて送信してください。",
+  };
+  res.render("hello", data);
+});
+
+router.post("/post", (req, res, next) => {
+  const msg = req.body["message"];
+  const data = {
+    title: "Hello!",
+    content: `あなたは、「${msg}」と送信しました。`,
   };
   res.render("hello", data);
 });
