c197b1954d7c1eb9acd7470a2655872c90d71050
unreact-hatano <mizutama.hahaha@gmail.com>
2023-07-18 22:01:08 +0900

    express入門　db.eachに処理を変更

--- ex-gen-app/routes/users-table.js
+++ ex-gen-app/routes/users-table.js
@@ -10,17 +10,24 @@ const db = new sqlite3.Database("mydb.sqlite3");
 router.get("/", (req, res, next) => {
   // データベースのシリアライズ
   db.serialize(() => {
-    // レコードを全て取り出す
-    db.all("select * from mydata", (err, rows) => {
-      // データベースアクセス完了時の処理
-      if (!err) {
-        let data = {
-          title: "Users Table!",
-          content: rows, // 取得したレコードデータ
-        };
-        res.render("users-table", data);
-      }
-    });
+    let rows = "";
+    db.each(
+      "select * from mydata",
+      (err, row) => {
+        if (!err) {
+          rows += `<tr><th>${row.id}</th><td>${row.name}</td></tr>`;
+        }
+      },
+      (err, count) => {
+        if (!err) {
+          const data = {
+            title: "Users Table!",
+            content: rows, // 取得したレコードデータ
+          };
+          res.render("users-table", data);
+        }
+      },
+    );
   });
 });
 
