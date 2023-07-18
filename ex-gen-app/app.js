2c353afecbae17f44f6980baabcd02fdb909afd0
unreact-hatano <mizutama.hahaha@gmail.com>
2023-07-18 12:43:56 +0900

    express入門　RSS取得

--- ex-gen-app/app.js
+++ ex-gen-app/app.js
@@ -1,12 +1,13 @@
 const createError = require("http-errors");
 const express = require("express");
-const session = require("express-session");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const helloRouter = require("./routes/hello");
+const holaRouter = require("./routes/hola");
 
 let app = express();
 const session_opt = {
@@ -30,6 +31,7 @@ app.use(session(session_opt));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/hello", helloRouter);
+app.use("/hola", holaRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ ex-gen-app/routes/hola.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const http = require("https");
+const parseString = require("xml2js").parseString;
+
+router.get("/", (req, res, next) => {
+  const opt = {
+    host: "news.google.com",
+    port: 443,
+    path: "/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja",
+  };
+  http.get(opt, (res2) => {
+    let body = "";
+    res2.on("data", (data) => {
+      body += data;
+    });
+    res2.on("end", () => {
+      parseString(body.trim(), (err, result) => {
+        console.log(result);
+        let data = {
+          title: "Google News",
+          content: result.rss.channel[0].item,
+        };
+        res.render("hola", data);
+      });
+    });
+  });
+});
+
+module.exports = router;
