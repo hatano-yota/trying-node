512992e986904acef24fe4cf01004ca1a61d3d21
unreact-hatano <mizutama.hahaha@gmail.com>
2023-08-06 16:20:36 +0900

    express入門　dbにデータを追加

--- ex-gen-app/routes/users-table.js
+++ ex-gen-app/routes/users-table.js
@@ -13,22 +13,41 @@ router.get("/", (req, res, next) => {
     let rows = "";
     db.each(
       "select * from mydata",
+      // レコードが取り出される毎に実行
       (err, row) => {
         if (!err) {
-          rows += `<tr><th>${row.id}</th><td>${row.name}</td></tr>`;
+          rows += `<tr><th>${row.id}</th><td>${row.name}</td><td>${row.mail}</td><td>${row.age}</td></tr>`;
         }
       },
+      // 全てのレコードを取り出し終わった後に実行
       (err, count) => {
         if (!err) {
           const data = {
             title: "Users Table!",
             content: rows, // 取得したレコードデータ
           };
-          res.render("users-table", data);
+          res.render("users-table/index", data);
         }
       },
     );
   });
+});
+
+router.get("/add", (req, res, next) => {
+  const data = {
+    title: "Hello/Add",
+    content: "新しいレコードを入力：",
+  };
+  res.render("users-table/add", data);
+});
+router.post("/add", (req, res, next) => {
+  const nm = req.body.name;
+  const ml = req.body.mail;
+  const ag = req.body.age;
+  db.serialize(() => {
+    db.run("insert into mydata (name, mail, age) values (?, ?, ?)", nm, ml, ag);
+  });
+  res.redirect("/users-table");
 });
 
 module.exports = router;
