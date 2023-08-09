396a43786f44b9e08b82c8524b7a71272528d231
unreact-hatano <mizutama.hahaha@gmail.com>
2023-08-09 14:02:42 +0900

    Opを使った条件指定

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const { Op } = require("sequelize");
 const router = express.Router();
 const db = require("../models/index");
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
-  db.User.findAll().then((users) => {
+  const { id, nm, min, max } = req.query;
+  const whereCondition = {
+    ...(id ? { id: { [Op.lte]: id } } : {}),
+    ...(nm ? { name: { [Op.like]: `%${nm}%` } } : {}),
+    ...(min && max ? { age: { [Op.gte]: min * 1, [Op.lte]: max * 1 } } : {}),
+  };
+  db.User.findAll({ where: whereCondition }).then((users) => {
     const data = {
       title: "Users/Index",
       content: users,
